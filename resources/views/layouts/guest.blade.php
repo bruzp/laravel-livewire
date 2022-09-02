a71f275bd11ed51b1ea3cba9d7ee502e3de4d697
Bruce <bruce@ipp.ph>
2022-09-02 14:13:21 +0800

    added authentication menus on front side

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,6 +19,24 @@
 </head>
 
 <body>
+    <div class="relative flex items-top justify-center sm:items-center sm:pt-0">
+        @if (Route::has('login'))
+            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block bg-gray-100 dark:bg-gray-900">
+                @auth
+                    <a href="{{ url('/dashboard') }}"
+                        class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                @else
+                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}"
+                            class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+    </div>
+
     <div class="font-sans text-gray-900 antialiased">
         {{ $slot }}
     </div>
