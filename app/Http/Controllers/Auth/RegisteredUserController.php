51c894b434a4536f0e3e71ab24ec7a2fc080ce9e
Bruce <bruce@ipp.ph>
2022-12-19 13:49:44 +0800

    fixed user does not have attached role during registration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -45,6 +45,8 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
+        $user->assignRole('user');
+
         event(new Registered($user));
 
         Auth::login($user);
